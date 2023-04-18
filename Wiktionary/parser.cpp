2ec513bf545c823786118a2a06373c4fa2a5ae4f
Axiomatic314 <rumkate289@gmail.com>
2023-04-18 18:35:38 +1200

    initial start to retrieving locations of relevant articles

--- Wiktionary/parser.cpp
+++ Wiktionary/parser.cpp
@@ -1,38 +1,60 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <string>
+#include <set>
 using namespace std;
 
-/**
- * Creates a list of relevant articles from the Wiktionary.
-*/
-vector<string> relevantArticles(vector<string> validWords, vector<string> articleIndex){
-    vector<string> results;
-    string currentWord = validWords.at(0);
-    int currentWordIndex = 0;
-    
-    for(string entry: articleIndex){
-        string currentEntry;
-        //find the article title - note that some languages are right to left, so may need to revist this
-        size_t found = entry.rfind(":");
-        if(found != string::npos){
-            currentEntry = entry.substr(found+1);
+struct block{
+    int seekBytes;
+    int totalBytes;
+    vector<int> articleIDs;
+};
+
+vector<block> getArticleList(set<string> dict){
+    vector<block> blockList;
+    string line;
+    string indexFilePath = "enwiktionary-20230401-pages-articles-multistream-index.txt";
+    fstream indexFile(indexFilePath, fstream::in);
+    if(!indexFile.is_open()){
+        fprintf(stderr, "Error opening article index!\n");
+        return blockList;
+    }
+    while(getline(indexFile, line)){
+        string delim = ":";
+        string token;
+        vector<string> lineSegments;
+        size_t next = 0;
+        size_t last = 0;
+        while((next = line.find(delim, last)) != string::npos){
+            token = line.substr(last, next-last);
+            last = next + 1;
+            lineSegments.push_back(token);
         }
-        int compareResults = currentEntry.compare(currentWord);
-        if(compareResults == 0){ //the entry is in our list of valid words
-            results.push_back(entry);
-            currentWordIndex++;
-        }else if(compareResults > 0){ //the entry is greater than our current valid word
-            while(compareResults >= 0 && currentWordIndex+1 <= validWords.size()){
-                currentWord = validWords.at(currentWordIndex++);
-                compareResults = currentEntry.compare(currentWord);
-                if(compareResults == 0){ //the entry is in our list of valid words
-                    results.push_back(entry);
-                    currentWordIndex++;
-                    break;
-                }
-            }
-        }
+        //process contents of line
     }
 
+}
+
+int main(){
+
+    //get a set of valid English words
+    set<string> dict; 
+    string word;
+    string wordFilePath = "words.txt";
+    fstream wordFile(wordFilePath, fstream::in);
+    if(!wordFile.is_open()){
+        fprintf(stderr, "Error opening word list!\n");
+        exit(EXIT_FAILURE);
+    }
+    while(getline(wordFile, word)){
+        dict.insert(word);
+    }
+    wordFile.close();
+
+    //parse index for info on relevant articles
+    vector<block> articleList = getArticleList(dict);
+
+
+    return EXIT_SUCCESS;
 }
