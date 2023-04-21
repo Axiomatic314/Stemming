1158cde3a7c89ef7d6975b374eae415e880dbd8a
Axiomatic314 <rumkate289@gmail.com>
2023-04-21 18:24:49 +1200

    prints the affix entries for english words

--- Wiktionary/words.py
+++ Wiktionary/words.py
@@ -2,11 +2,20 @@ import json
 
 filename = "raw-wiktextract-data.json"
 data: dict = None
+words = list()
 
 with open(filename, "r", encoding="utf-8") as f:
     for line in f:
         data = json.loads(line)
-        #Process the data accordingly
-        #For example:
-        if "lang" in data.keys():
-            if data["lang"] != "English": print(data["lang"])
+        if "lang" in data.keys() and data["lang"] == "English":
+            words.append(data)
+
+for word in words:
+    if "etymology_templates" not in word.keys():
+        continue
+    for template in word["etymology_templates"]:
+        name = template["name"]
+        if name == "affix" or name == "af":
+            print(word["word"] + " ")
+            for arg in template["args"]:
+                print(str(arg) + " " + template["args"][arg])
