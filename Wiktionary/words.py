ea35b271bce169b9fa257fe0cf3e0a7650d2580e
Axiomatic314 <rumkate289@gmail.com>
2023-04-23 15:48:17 +1200

    produces rough list of word: replacement

--- Wiktionary/words.py
+++ Wiktionary/words.py
@@ -10,12 +10,45 @@ with open(filename, "r", encoding="utf-8") as f:
         if "lang" in data.keys() and data["lang"] == "English":
             words.append(data)
 
+word_variants = dict()
 for word in words:
     if "etymology_templates" not in word.keys():
         continue
+    variants = list()
     for template in word["etymology_templates"]:
         name = template["name"]
+        if "1" in template["args"] and template["args"]["1"] != "en":
+            continue
+        #If there is an override code for a language that is not English, skip it
+        if "2" in template["args"]:
+            temp = template["args"]["2"].split(":")
+            if len(temp) > 1 and temp[0] != "en": 
+                continue
+            elif len(temp) > 1:
+                template["args"]["2"] = temp[1]
+        if "3" in template["args"]:
+            temp = template["args"]["3"].split(":")
+            if len(temp) > 1 and temp[0] != "en": 
+                continue
+            elif len(temp) > 1:
+                template["args"]["3"] = temp[1]
+        #Obtain the root if able
         if name == "affix" or name == "af":
-            print(word["word"] + " ")
-            for arg in template["args"]:
-                print(str(arg) + " " + template["args"][arg])
+            if "2" in template["args"] and "3" in template["args"]:
+                if template["args"]["2"].endswith("-") and not template["args"]["3"].startswith("-"): #prefix
+                    variants.append(template["args"]["3"])
+                elif template["args"]["3"].startswith("-") and not template["args"]["2"].endswith("-"): #suffix
+                    variants.append(template["args"]["2"])
+        if name == "prefix" or name == "pre":
+            if "3" in template["args"]: 
+                variants.append(template["args"]["3"])
+        if name == "suffix" or name == "suf":
+            if "2" in template["args"]: 
+                variants.append(template["args"]["2"])
+    if len(variants) > 0:
+        word_variants[word["word"]] = variants
+
+for entry in word_variants:
+    print(entry + ": ", end="")
+    for variant in word_variants[entry]:
+        print(variant, sep=", ")
