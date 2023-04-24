b7587df6824ec94bd9632d7f9b1442e6da06b3ba
Katelyn <rumkate289@gmail.com>
2023-04-24 13:51:03 +1200

    tidied up code, added some checks to make sure the components of each word are valid

--- Wiktionary/words.py
+++ Wiktionary/words.py
@@ -1,10 +1,10 @@
 import json
 
 filename = "raw-wiktextract-data.json"
-data: dict = None
-words = list()
-word_variants = dict()
+words = list() #list of all the English words in the Wiktionary
+word_variants = dict() #contains all the word:replacment pairs
 
+data: dict = None
 with open(filename, "r", encoding="utf-8") as f:
     for line in f:
         data = json.loads(line)
@@ -14,46 +14,59 @@ with open(filename, "r", encoding="utf-8") as f:
 for word in words:
     if "etymology_templates" not in word.keys():
         continue
+
+    #If the word is a person's name, we leave it as it is
+    isName = False
+    if "categories" in word.keys():
+        for category in word["categories"]:
+            if category.find("surnames") > -1 or category.find("given names") > -1:
+                isName = True
+                break
+    if isName: continue
+
     for template in word["etymology_templates"]:
-        name = template["name"]
-        if "1" in template["args"] and template["args"]["1"] != "en":
+        valid_lang_codes = ["en"]
+        if "1" in template["args"] and template["args"]["1"] not in valid_lang_codes:
             continue
-        #If there is an override code for a language that is not English, skip it
-        #Deal with any language codes included in the components themselves
-        if "2" in template["args"]:
-            temp = template["args"]["2"].split(":")
-            if len(temp) > 1 and temp[0] != "en": 
+ 
+        #Only process the entry if the main word components are provided
+        if "2" not in template["args"] or "3" not in template["args"]:
+            continue
+        if len(template["args"]["2"]) == 0 or len(template["args"]["3"]) == 0:
+            continue
+
+        #Deal with any language codes included in the word components
+        temp = template["args"]["2"].split(":")
+        if len(temp) > 1:
+            if temp[0] not in valid_lang_codes or temp[0].find("<") > -1:
                 continue
-            elif len(temp) > 1:
-                template["args"]["2"] = temp[1]
-        if "3" in template["args"]:
-            temp = template["args"]["3"].split(":")
-            if len(temp) > 1 and temp[0] != "en": 
+            template["args"]["2"] = temp[1]
+        temp = template["args"]["3"].split(":")
+        if len(temp) > 1:
+            if temp[0] not in valid_lang_codes or temp[0].find("<") > -1:
                 continue
-            elif len(temp) > 1:
-                template["args"]["3"] = temp[1]
-        #Obtain the root if able
+            template["args"]["3"] = temp[1]
+            
+        #Process the template    
+        negative_prefixes = ["anti", "anti-", "ab", "ab-", "non", "non-", "un", "un-"]
+        negative_suffixes = ["n't", "n't-"]
+        name = template["name"]
+        part1 = template["args"]["2"]
+        part2 = template["args"]["3"]
         if name == "affix" or name == "af":
-            if "2" in template["args"] and "3" in template["args"]:
-                if template["args"]["2"].endswith("-") and not template["args"]["3"].startswith("-"): #prefix
-                    word_variants[word["word"]] = template["args"]["3"]
-                elif template["args"]["3"].startswith("-") and not template["args"]["2"].endswith("-"): #suffix
-                    word_variants[word["word"]] = template["args"]["2"]
+            if part1.endswith("-") and not part2.startswith("-"): #prefix
+                if part1 not in negative_prefixes:
+                    word_variants[word["word"]] = part2
+            elif part2.startswith("-") and not part1.endswith("-"): #suffix
+                if part2 not in negative_suffixes: 
+                    word_variants[word["word"]] = part1
         if name == "prefix" or name == "pre":
-            if "3" in template["args"]: 
-                word_variants[word["word"]] = template["args"]["3"]
+            if part1 not in negative_prefixes:
+                word_variants[word["word"]] = part2
         if name == "suffix" or name == "suf":
-            if "2" in template["args"]: 
-                word_variants[word["word"]] = template["args"]["2"]
-
-# for entry in word_variants:
-#     print(entry + ": ", end="")
-#     if len(word_variants[entry]) < 1:
-#         print("\n")
-#         continue
-#     for variant in word_variants[entry]:
-#         print(variant, sep=", ")
-
+            if part2 not in negative_suffixes:
+                word_variants[word["word"]] = part1
+       
 for word in word_variants:
     print(word + ": ", end="")
     print(word_variants[word])
