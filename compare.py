612a21fa9cf4b74ac71d1e39e6f8d141044e1a78
Katelyn <rumkate289@gmail.com>
2023-05-03 14:39:37 +1200

    clusters are made from word->stem files now, also made a start to measuring accuracy

--- compare.py
+++ compare.py
@@ -1,7 +1,28 @@
 import sys
+import os
+import nltk.metrics.distance as distance
 
-def create_cluster_list(filename):
-    #todo: change this to use the word -> stem file, so can get rid of cluster.py entirely?
+def create_clusters(filename):
+    cluster_list = list()
+    stem_clusters = dict()
+    stem_pairs = list()
+    with open(filename, "r", encoding="utf-8") as f:
+        for line in f:
+            contents = line.split(sep="->")
+            stem_pairs.append((contents[0].strip(), contents[1].strip()))
+    #Find which words map to the same stem
+    for word, stem in stem_pairs:
+        if stem not in stem_clusters.keys():
+            stem_clusters[stem] = list()
+        stem_clusters[stem].append(word)
+    #Get list of the clusters
+    for stem in stem_clusters:
+        cluster = list()
+        cluster = stem_clusters[stem]
+        cluster_list.append(cluster)
+    return cluster_list, stem_pairs
+
+def get_clusters(filename):
     cluster_list = list()
     with open(filename, "r", encoding="utf-8") as f:
         for line in f:
@@ -24,7 +45,6 @@ def print_size_distribution(clusters, filename):
         for size in cluster_sizes:
             f.write("(" + str(size) + ", " + str(cluster_sizes[size]) + ")\n")
 
-
 def print_clusters(clusters, filename):
     with open(filename, "w", encoding="utf-8") as f:
         for cluster in clusters:
@@ -32,16 +52,36 @@ def print_clusters(clusters, filename):
                 f.write(word + " ")
             f.write("\n")
 
-def main():
-    stemmer_file = "Stemmers/Snowball/snowballClusters.txt"
-    wikt_file = "Wiktionary/reducedGroupings.txt"
+def measure_word_accuracy(wikt_pairs, stem_pairs):
+    scores = list()
+    for i in range (0, len(stem_pairs)):
+        #may want to add more of a penalty to substitions??
+        scores.append(distance.edit_distance(wikt_pairs[i][1], stem_pairs[i][1]))
+    #return an average
+    
 
-    #Get the clusters
-    stem_clusters = create_cluster_list(stemmer_file)
-    wikt_clusters = create_cluster_list(wikt_file)
+
+def measure_cluster_accuracy(wikt_clusters, stem_clusters):
+    #determine the accuracy of the clusters....
+    return 0
+
+def main():
+    #todo: check if the necessary files are actually there
+    wikt_file = "Wiktionary/replacements.txt"
+    #Todo: change the stemmer file to be given via command line?
+    stemmer_file = "Stemmers/Snowball/snowballOutput.txt"
+
+    # Get the clusters from a preclustered file
+    # wikt_file = "Wiktionary/clusters.txt"
+    # wikt_clusters = get_clusters(wikt_cluster_file)
+
+    # Get the clusters from word->stem files
+    wikt_clusters, wikt_pairs = create_clusters(wikt_file)
+    stem_clusters, stem_pairs = create_clusters(stemmer_file)
     print(f"Total clusters for the stemmer: {len(stem_clusters)}", file=sys.stderr)
     print(f"Total clusters for the wiktionary: {len(wikt_clusters)}", file=sys.stderr)
 
+    # Get the distribution of cluster sizes 
     print_size_distribution(stem_clusters, "stem_distribution")
     print_size_distribution(wikt_clusters, "wikt_distribution")
 
@@ -51,8 +91,11 @@ def main():
     print(f"Clusters for the stemmer with length > 1: {len(stem_clusters)}", file=sys.stderr)
     print(f"Clusters for the wiktionary with length > 1: {len(wikt_clusters)}", file=sys.stderr)
 
+    #Included for testing
     print_clusters(wikt_clusters, "wikt_clusters.txt")
     print_clusters(stem_clusters, "stem_clusters.txt")
+
+
 
 if __name__ == "__main__":
     main()
