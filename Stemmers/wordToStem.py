c04fde4c08fb0a5a2502b01d78e086efc6cbaea9
Axiomatic314 <rumkate289@gmail.com>
2023-06-04 17:36:48 +1200

    gets words and their stems from ATIRE output

---
+++ Stemmers/wordToStem.py
@@ -0,0 +1,46 @@
+
+def getWords(wordListFile):
+    words = list()
+    with open(wordListFile, "r") as f:
+        for line in f:
+            words.append(line[:-1])
+    return words
+
+def getStems(numWords, stemListFile):
+    lines = list()
+    with open(stemListFile, "r") as f:
+        for line in f:
+            lines.append(line)
+    if lines[0].startswith("This"): lines.pop(0)
+    stems = [""] * numWords
+    stem = ""
+    for line in lines:
+        if line[0][0].isalpha():
+            stem = line[:-2]
+        elif line[0][0] == "<":
+            docid = int(line[1:line.find(",")])
+            stems[docid] = stem
+        else:
+            return stems
+        
+def printResults(words, stems, outputFile):
+    with open(outputFile, "w") as f:
+        for i in range(0,len(words)):
+            f.write(words[i] + " -> " + stems[i] + "\n")
+    return
+
+def main():
+    wordListFile = "/home/katelyn/Documents/COSC490/temp/doclist.aspt"
+    stemListFile = "/home/katelyn/Documents/COSC490/temp/dictionary"
+    outputFile = "Stemmers/output"
+
+    print("Processing doclist...")
+    words = getWords(wordListFile)
+    print("Processing dictionary...")
+    stems = getStems(len(words), stemListFile)
+    print("Printing results to file...")
+    printResults(words, stems, outputFile)
+    print("Complete!")
+
+if __name__ == "__main__":
+    main()
