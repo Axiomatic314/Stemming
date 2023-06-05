4941ac52bf04e611f2f1ca48859e7bfbd12fc783
Axiomatic314 <rumkate289@gmail.com>
2023-06-05 20:42:49 +1200

    quick script to gather data

---
+++ runCompare.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+python compare.py ~/Documents/COSC490/Stemming/Stemmers/krovetz/wikt krovetz
+python compare.py ~/Documents/COSC490/Stemming/Stemmers/lovins/wikt lovins
+python compare.py ~/Documents/COSC490/Stemming/Stemmers/sStripping/wikt sStripping
+python compare.py ~/Documents/COSC490/Stemming/Stemmers/paiceHusk/wikt paiceHusk
+python compare.py ~/Documents/COSC490/Stemming/Stemmers/porter2/wikt porter2
