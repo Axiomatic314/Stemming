19d15c3b5223628897ab8d1830ee6239059e542d
Axiomatic314 <rumkate289@gmail.com>
2023-06-07 00:38:24 +1200

    tidied up script

--- runCompare.sh
+++ runCompare.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
-python compare.py ~/Documents/COSC490/Stemming/Stemmers/krovetz/wikt krovetz
-python compare.py ~/Documents/COSC490/Stemming/Stemmers/lovins/wikt lovins
-python compare.py ~/Documents/COSC490/Stemming/Stemmers/sStripping/wikt sStripping
-python compare.py ~/Documents/COSC490/Stemming/Stemmers/paiceHusk/wikt paiceHusk
-python compare.py ~/Documents/COSC490/Stemming/Stemmers/porter2/wikt porter2
+
+STEMMING_PATH='/home/katelyn/Documents/COSC490/Stemming'
+
+python compare.py $STEMMING_PATH/Stemmers/krovetz/wikt krovetz
+python compare.py $STEMMING_PATH/Stemmers/lovins/wikt lovins
+python compare.py $STEMMING_PATH/Stemmers/sStripping/wikt sStripping
+python compare.py $STEMMING_PATH/Stemmers/paiceHusk/wikt paiceHusk
+python compare.py $STEMMING_PATH/Stemmers/porter2/wikt porter2
