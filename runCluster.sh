db0a91b432f74ad6686e4c959b0dc4849c2905d5
Katelyn <rumkate289@gmail.com>
2023-11-14 11:50:29 +1300

    removed old scripts

--- runCluster.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-STEMMING_PATH='/home/katelyn/Documents/COSC490/Stemming'
-
-python cluster.py $STEMMING_PATH/Stemmers/krovetz/wikt krovetz y
-python cluster.py $STEMMING_PATH/Stemmers/lovins/wikt lovins y
-python cluster.py $STEMMING_PATH/Stemmers/sStripping/wikt sStripping y
-python cluster.py $STEMMING_PATH/Stemmers/paiceHusk/wikt paiceHusk y
-python cluster.py $STEMMING_PATH/Stemmers/porter2/wikt porter2 y
--- runCompare.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-STEMMING_PATH='/home/katelyn/Documents/COSC490/Stemming'
-
-python compare.py $STEMMING_PATH/Stemmers/krovetz/wikt krovetz
-python compare.py $STEMMING_PATH/Stemmers/lovins/wikt lovins
-python compare.py $STEMMING_PATH/Stemmers/sStripping/wikt sStripping
-python compare.py $STEMMING_PATH/Stemmers/paiceHusk/wikt paiceHusk
-python compare.py $STEMMING_PATH/Stemmers/porter2/wikt porter2
