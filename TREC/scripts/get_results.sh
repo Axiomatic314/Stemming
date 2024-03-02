dc83c2fad753e48815b397c5a3896d092c0b8931
Katelyn <rumkate289@gmail.com>
2024-03-02 16:33:40 +1300

    fixed bug

--- TREC/scripts/get_results.sh
+++ TREC/scripts/get_results.sh
@@ -20,7 +20,7 @@ cd $ATIRE_PATH
 
 for i in "${!stemmer[@]}"
 do
-	./bin/atire -t"${stem[$i]}" -RBM25:"${k1[$i]}":"${b[$i]}" -findex $TREC_PATH/TREC\-$SUB/TREC\-$SUB.aspt -q$TREC_PATH/TREC\-$SUB/topics.$LOW\-$HIGH -a$TREC_PATH/TREC\-$SUB/$LOW\-$HIGH.qrels -et -o$TREC_PATH/output/TREC\-$SUB\-"${stemmer[$s]}".out
+	./bin/atire -t"${stem[$i]}" -RBM25:"${k1[$i]}":"${b[$i]}" -findex $TREC_PATH/TREC\-$SUB/TREC\-$SUB.aspt -q$TREC_PATH/TREC\-$SUB/topics.$LOW\-$HIGH -a$TREC_PATH/TREC\-$SUB/$LOW\-$HIGH.qrels -et -o$TREC_PATH/output/TREC\-$SUB\-"${stemmer[$i]}".out
 done
 
 cd $EVAL_PATH
@@ -29,7 +29,7 @@ echo "stemmer qid map" > $TREC_PATH/map/tuned_map_TREC\-$SUB
 for s in "${stemmer[@]}"
 do
 	: > $TREC_PATH/map/tuned_map_TREC\-$SUB\-temp
-	./trec_eval $TREC_PATH/TREC-$SUB/$LOW\-$HIGH.qrels $TREC_PATH/output/TREC\-$SUB\-$s.out -q -m map | cut -f 2,3 >> $TREC_PATH/map/tuned_map_TREC\-$SUB
+	./trec_eval $TREC_PATH/TREC-$SUB/$LOW\-$HIGH.qrels $TREC_PATH/output/TREC\-$SUB\-$s.out -q -m map | cut -f 2,3 >> $TREC_PATH/map/tuned_map_TREC\-$SUB\-temp
 	while read -r line
 	do
 		echo "${s} $line"
